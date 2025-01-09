99858f44c4867a5ef4838019739e115b09eade6f
songyu <2285329647@qq.com>
2025-01-09 16:33:34 +0800

    test

--- Solution.py
+++ Solution.py
@@ -1,17 +1,4 @@
-from typing import List
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        left = 0;
-        right=1;
-        maxSum=nums[0]
-        sum=nums[0]
-        while right<len(nums):
-            if nums[left]<=0:
-                sum-=nums[left]
-                left+=1
-                maxSum=max(sum,maxSum)
-            if nums[right]>0:
-                sum+=nums[right]
-                right+=1
-                maxSum=max(sum,maxSum)
-        
+import datetime
+from typing import Generator
+a="测试"*60
+print(a)
